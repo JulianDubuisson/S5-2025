da784ac83fffb1fdea89bb22be80612f2082b95e
jdubuisson <jdubuisson@bdsnet.com.ar>
2025-03-05 14:17:12 +0000

    first commit

---
+++ Clases/CLASE1.py
@@ -0,0 +1,12 @@
+nombre = "Julian"
+edad = 26
+NOMBRE2 = "Pedro" 
+
+
+
+print("Mi nombre es", nombre, "mi edad es", edad)
+
+name = input("¿Cúal es tu nombre?: ")
+edad = int(input("¿Cual es tu edad: "))
+print ("Tu nombre es", name, "y tenes ", edad, "años")
+
---
+++ Clases/CLASE2.py
@@ -0,0 +1,49 @@
+""" a=7 
+b=2
+
+x=a 
+x+=b  
+print("x+=", x) 
+
+
+x=a 
+x-=b
+print("x-=", x)  
+
+
+x=a; 
+x*=b;  
+print("x*=", x)  
+
+x=a; 
+x/=b;  
+print("x/=", x)  """
+
+""" numero1 = 5
+numero2 = 6
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+ """
+
+
+""" ------------------------------------------------------------ """
+
+print("***CALCULADORA CLIMATICA***")
+
+celsius = float(input("Ingrese los grados en C°: "))
+
+farenheit= (celsius * 9 / 5) + 32
+
+print ("Los grados en F", farenheit)
+
+
